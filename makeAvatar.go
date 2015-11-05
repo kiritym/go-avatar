a764214229d200eac7726b1c0e087315565f9f81
Kirity <Kiritym@rssoftware.co.in>
2015-11-05 18:31:44 +0530

    separate pattern for top and bottom half

--- makeAvatar.go
+++ makeAvatar.go
@@ -3,25 +3,87 @@ package main
 import (
 	"fmt"
 	"image"
-	"os"
+	//"os"
+	"net/http"
+	"image/jpeg"
+	"bytes"
+	"log"
+	"strconv"
+	"encoding/base64"
+	"html/template"
 )
 
 const (
-	AvatarSize = 120
+	AvatarSize = 240
 	PixelSize = AvatarSize/12
 )
 
+
 func main() {
-	fmt.Println(os.Args[1])
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go-avatar username")
-		return
+
+	http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
+	log.Println("Listening on 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+  nameBytes := []byte(r.URL.Path[1:])
+  //fmt.Println(r.URL.Path[1:])
+	//token := r.URL.Query().Get("type")
+	//fmt.Println(token)
+  //fmt.Println(nameBytes)
+	if(len(nameBytes) < 3){
+		fmt.Fprintf(w, "Please provide a valid username(atleast 3 characters)")
+	}else{
+		avatar := image.NewRGBA(image.Rect(0, 0, AvatarSize, AvatarSize))
+		PaintBackGround(avatar, CalculateBGColor(nameBytes))
+		DrawPattern(avatar, nameBytes, CalculatePixelColor(nameBytes))
+		//SavePNG(avatar, r.URL.Path[1:])
+		var img image.Image = avatar
+		writeImageWithTemplate(w, &img)
 	}
 
-	nameBytes := []byte(os.Args[1])
-	fmt.Println(nameBytes)
-	avatar := image.NewRGBA(image.Rect(0, 0, AvatarSize, AvatarSize))
-	PaintBackGround(avatar, CalculateBGColor(nameBytes))
-	DrawPattern(avatar, nameBytes, CalculatePixelColor(nameBytes))
-	SavePNG(avatar)
+}
+
+var ImageTemplate string = `<!DOCTYPE html>
+<html lang="en"><head></head>
+<body><img src="data:image/jpg;base64,{{.Image}}"></body>`
+
+// encodes an image 'img' in jpeg format and writes it into ResponseWriter using a template.
+func writeImageWithTemplate(w http.ResponseWriter, img *image.Image) {
+
+	buffer := new(bytes.Buffer)
+	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+		log.Fatalln("unable to encode image.")
+	}
+
+	str := base64.StdEncoding.EncodeToString(buffer.Bytes())
+	if tmpl, err := template.New("image").Parse(ImageTemplate); err != nil {
+		log.Println("unable to parse image template.")
+	} else {
+		data := map[string]interface{}{"Image": str}
+		if err = tmpl.Execute(w, data); err != nil {
+			log.Println("unable to execute template.")
+		}
+	}
+}
+
+// encodes an image 'img' in jpeg format and writes it into ResponseWriter.
+func writeImage(w http.ResponseWriter, img *image.Image) {
+
+	buffer := new(bytes.Buffer)
+	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+		log.Println("unable to encode image.")
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+	if _, err := w.Write(buffer.Bytes()); err != nil {
+		log.Println("unable to write image.")
+	}
 }
--- patternHandler.go
+++ patternHandler.go
@@ -9,8 +9,8 @@ import (
 	"os"
 )
 
-func SavePNG(avatar image.Image) {
-	file, err := os.Create(os.Args[1] + ".png")
+func SavePNG(avatar image.Image, name string) {
+	file, err := os.Create(name + ".png")
 	err = png.Encode(file, avatar)
 
 	if err != nil {
@@ -24,42 +24,13 @@ func DrawPattern(avatar *image.RGBA, nameBytes []byte, pixelColor color.RGBA) {
 	// Random number based on the username.
 	var nameSum int64
 	for i := range nameBytes {
-		nameSum += int64(nameBytes[i])
+		nameSum += int64(nameBytes[i])*int64(i)
 	}
-	fmt.Println("nameSum:: ", nameSum)
-
+	fmt.Println("NameSum: ", nameSum)
 	// Use nameSum number to keep random-ness deterministic for a given name
 	rand.Seed(nameSum)
-
-	// Make the "pattern"
-	for y := PixelSize; y < AvatarSize/2 - PixelSize; y=y+PixelSize {
-		for x := PixelSize; x < AvatarSize/2 - PixelSize; x=x+PixelSize {
-			if ((x + y) % 2) == 0 {
-				if (rand.Intn(3) == 1) || (rand.Intn(3) == 2){
-					fmt.Printf("x= %d, y= %d\n", x,y)
-					fillPixels(avatar, x, y, pixelColor)
-				}
-			}
-		}
-	}
-
-	// Mirror top left half to right top half
-	for y := 0; y < AvatarSize; y++ {
-		for x := 0; x < AvatarSize; x++ {
-			if x < AvatarSize/2 {
-				avatar.Set(AvatarSize-x-1, y, avatar.At(x, y))
-			}
-		}
-	}
-
-	// Mirror top half to bottom half
-	for y := 0; y < AvatarSize; y++ {
-		for x := 0; x < AvatarSize; x++ {
-			if y < AvatarSize/2 {
-				avatar.Set(x, AvatarSize-y-1, avatar.At(x, y))
-			}
-		}
-	}
+	DrawTopPattern(avatar, pixelColor)
+	DrawBottomPattern(avatar, pixelColor)
 }
 
 
@@ -94,11 +65,66 @@ func CalculatePixelColor(nameBytes []byte) (pixelColor color.RGBA) {
 func CalculateBGColor(nameBytes []byte) (bgColor color.RGBA) {
 	bgColor.A = 255
 
-	var mutator = byte((len(nameBytes) * 3))
+	var mutator = byte((len(nameBytes) * 6))
 
 	bgColor.R = nameBytes[0] * mutator
 	bgColor.G = nameBytes[1] * mutator
 	bgColor.B = nameBytes[2] * mutator
 
 	return
+}
+
+func DrawTopPattern(avatar *image.RGBA, pixelColor color.RGBA) {
+	// Make the top left quadrant pattern
+	for y := PixelSize; y < AvatarSize/2 - PixelSize; y=y+PixelSize {
+		for x := PixelSize; x < AvatarSize/2 - PixelSize; x=x+PixelSize {
+			if ((x + y) % 2) == 0 {
+				randNumber := rand.Intn(3)
+				if (randNumber == 1) || (randNumber == 2){
+					//fmt.Printf("x= %d, y= %d\n", x,y)
+					fillPixels(avatar, x, y, pixelColor)
+				}
+			}
+		}
+	}
+	DrawTopRightPattern(avatar)
+}
+
+func DrawTopRightPattern(avatar *image.RGBA) {
+	// Mirror top left quadrant to right top quadrant
+	for y := 0; y < AvatarSize; y++ {
+		for x := 0; x < AvatarSize; x++ {
+			if x < AvatarSize/2 {
+				avatar.Set(AvatarSize-x-1, y, avatar.At(x, y))
+			}
+		}
+	}
+}
+
+func DrawBottomPattern(avatar *image.RGBA, pixelColor color.RGBA) {
+	// Make the bottom left pattern
+	for y := AvatarSize/2 + PixelSize; y < AvatarSize - PixelSize; y=y+PixelSize {
+		for x := PixelSize; x < AvatarSize/2 - PixelSize; x=x+PixelSize {
+			if ((x + y) % 2) == 0 {
+				randNumber := rand.Intn(3)
+				//fmt.Println(randNumber)
+				if ((randNumber == 0) || (randNumber == 1)){
+					//fmt.Printf("x= %d, y= %d\n", x,y)
+					fillPixels(avatar, x, y, pixelColor)
+				}
+			}
+		}
+	}
+	DrawBottomRightPattern(avatar)
+}
+
+func DrawBottomRightPattern(avatar *image.RGBA) {
+	// Mirror bottom left quadrant to right bottom quadrant
+	for y := AvatarSize/2; y < AvatarSize; y++ {
+		for x := 0; x < AvatarSize; x++ {
+			if (x < AvatarSize/2 ) {
+				avatar.Set(AvatarSize-x-1, y, avatar.At(x, y))
+			}
+		}
+	}
 }
